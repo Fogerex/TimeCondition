3fc0ff5650918a1d054bca4d69f23ddf98104291
Fogerex <fogerex@yandex.ru>
2023-04-13 18:47:06 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"github.com/atotto/clipboard"
 )
 
 func main() {
@@ -37,13 +38,17 @@ func main() {
 	//Create MultiLineEntry
 	output := widget.NewMultiLineEntry()
 	if datetime.Year() != datetime2.Year() || datetime.Month() != datetime2.Month() || datetime.Day() != datetime2.Day() {
-		output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", datetime.Month(), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()>date(", datetime.Year(), ",", datetime.Month(), ",", datetime.Day(), ")&&date()<date(", datetime2.Year(), ",", datetime2.Month(), ",", datetime2.Day(), "))||\n(date()=date(", datetime2.Year(), ",", datetime2.Month(), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
+		output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", int(datetime.Month()), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()>date(", datetime.Year(), ",", int(datetime.Month()), ",", datetime.Day(), ")&&date()<date(", datetime2.Year(), ",", int(datetime2.Month()), ",", datetime2.Day(), "))||\n(date()=date(", datetime2.Year(), ",", int(datetime2.Month()), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
 	} else {
-		output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", datetime.Month(), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()=date(", datetime2.Year(), ",", datetime2.Month(), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
+		output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", int(datetime.Month()), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()=date(", datetime2.Year(), ",", int(datetime2.Month()), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
 	}
 
+	btn := widget.NewButton("Копировать", func() {
+		clipboard.WriteAll(output.Text)
+	})
+
 	// Create a horizontal box container to hold the date and time picker and the label
-	dateTimeBox := container.NewVBox(startbox, endbox, output)
+	dateTimeBox := container.NewVBox(startbox, endbox, output, btn)
 
 	// Create a vertical box container to hold the horizontal box container and the frame
 	content := container.NewVBox(dateTimeBox)
@@ -70,9 +75,9 @@ func main() {
 
 			}
 			if datetime.Year() != datetime2.Year() || datetime.Month() != datetime2.Month() || datetime.Day() != datetime2.Day() {
-				output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", datetime.Month(), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()>date(", datetime.Year(), ",", datetime.Month(), ",", datetime.Day(), ")&&date()<date(", datetime2.Year(), ",", datetime2.Month(), ",", datetime2.Day(), "))||\n(date()=date(", datetime2.Year(), ",", datetime2.Month(), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
+				output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", int(datetime.Month()), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()>date(", datetime.Year(), ",", int(datetime.Month()), ",", datetime.Day(), ")&&date()<date(", datetime2.Year(), ",", int(datetime2.Month()), ",", datetime2.Day(), "))||\n(date()=date(", datetime2.Year(), ",", int(datetime2.Month()), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
 			} else {
-				output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", datetime.Month(), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()=date(", datetime2.Year(), ",", datetime2.Month(), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
+				output.SetText(fmt.Sprint("(date()=date(", datetime.Year(), ",", int(datetime.Month()), ",", datetime.Day(), ")&&time()>=time(", datetime.Hour(), ",", datetime.Minute(), "))||\n(date()=date(", datetime2.Year(), ",", int(datetime2.Month()), ",", datetime2.Day(), ")&&time()<=time(", datetime2.Hour(), ",", datetime2.Minute(), "))"))
 			}
 			time.Sleep(time.Second)
 
